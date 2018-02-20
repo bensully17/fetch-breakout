620198416ef18c4f23db3c20e4ab41ef3cb4d663
Benjamin Sullivan <ben@Benjamins-iMac.local>
2018-02-19 17:17:37 -0700

    update 2

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-// GET request
-  // Request to get information from an API
-  // Different API endpoints => different data returned
-
 var baseUrl = 'http://services.swpc.noaa.gov'
 var time = ''
 setTimeout(function(){
   location = ''
-},30000)
+},60000)
+
+function northHemi() {
+  fetch(baseUrl + '/images/animations/ovation-north/latest.jpg')
+  .then(function(result){
+    ovationImage.setAttribute('src', result.url)
+    ovationImage.setAttribute('id', 'ovImg')
+  })
+}
+function southHemi() {
+  fetch(baseUrl + '/images/animations/ovation-south/latest.jpg')
+  .then(function(result){
+    ovationImage.setAttribute('src', result.url)
+    ovationImage.setAttribute('id', 'ovImg')
+  })
+}
+northHemi()
 
 for (let i = 0; i < 3; i++) {
   let main = document.querySelector('main')
@@ -20,35 +32,41 @@ newSections[1].setAttribute('id', 'data')
 newSections[2].setAttribute('id', 'charts')
 
 let mainOptions = document.querySelector('#options')
-for ()
+// for ()
 
 for (let i = 0; i < 4; i++) {
   let mainData = document.querySelector('#data')
   let newDiv = document.createElement('div')
   mainData.appendChild(newDiv)
-  for (let j = 0; j < 2; j++) {
+  for (let j = 0; j < 3; j++) {
     var newInnerDiv = document.createElement('div')
     let newDivs = document.querySelectorAll('#data > div')
     newDivs[i].appendChild(newInnerDiv)
   }
 }
 let charts = document.querySelector('#charts')
-for (i = 0; i < 1; i++) {
+for (i = 0; i < 2; i++) {
   let newChartDiv = document.createElement('div')
   charts.appendChild(newChartDiv)
 }
 let ovation = document.querySelectorAll('#charts > div')[0]
 let ovationImage = document.createElement('img')
-
 ovation.appendChild(ovationImage)
-
-fetch(baseUrl + '/images/animations/ovation-north/latest.jpg')
-  .then(function(result){
-    ovationImage.setAttribute('src', result.url)
-    ovationImage.setAttribute('id', 'ovImg')
-  })
+ovation.setAttribute('id', 'ovation')
 
 
+for (i = 0; i < 2; i++){
+  let buttonsDiv = document.querySelectorAll('#charts > div')[1]
+  buttonsDiv.setAttribute('id', 'buttons')
+  let newButtons = document.createElement('button')
+  newButtons.setAttribute('class', 'button')
+  buttonsDiv.appendChild(newButtons)
+}
+const buttons = document.querySelectorAll('.button')
+buttons[0].textContent = 'Northern Hemisphere'
+buttons[1].textContent = 'Southern Hemisphere'
+buttons[0].addEventListener('click', northHemi)
+buttons[1].addEventListener('click', southHemi)
 
 newDivs = document.querySelectorAll('#data > div')
 newDivs[0].setAttribute('id', 'speed')
@@ -57,30 +75,44 @@ newDivs[2].setAttribute('id', 'bz')
 newDivs[3].setAttribute('id', 'intensity')
 
 let speedText = document.querySelectorAll('#speed > div')
-speedText[0].setAttribute('id', 'key')
-speedText[1].setAttribute('id', 'value')
+speedText[0].setAttribute('class', 'key')
+speedText[1].setAttribute('class', 'value')
+speedText[2].setAttribute('class', 'units')
 
 let densityText = document.querySelectorAll('#density > div')
-densityText[0].setAttribute('id', 'key')
-densityText[1].setAttribute('id', 'value')
+densityText[0].setAttribute('class', 'key')
+densityText[1].setAttribute('class', 'value')
+densityText[2].setAttribute('class', 'units')
 
 let bzText = document.querySelectorAll('#bz > div')
-bzText[0].setAttribute('id', 'key')
-bzText[1].setAttribute('id', 'value')
+bzText[0].setAttribute('class', 'key')
+bzText[1].setAttribute('class', 'value')
+bzText[2].setAttribute('class', 'units')
 
 let intensityText = document.querySelectorAll('#intensity > div')
-intensityText[0].setAttribute('id', 'key')
-intensityText[1].setAttribute('id', 'value')
+intensityText[0].setAttribute('class', 'key')
+intensityText[1].setAttribute('class', 'value')
 
-let speedKey = document.querySelector('#speed > #key')
-let densityKey = document.querySelector('#density > #key')
-let bzKey = document.querySelector('#bz > #key')
-let intensityKey = document.querySelector('#intensity > #key')
+let speedKey = document.querySelector('#speed > .key')
+let densityKey = document.querySelector('#density > .key')
+let bzKey = document.querySelector('#bz > .key')
+let intensityKey = document.querySelector('#intensity > .key')
 
 speedKey.textContent = 'Solar Wind Speed:'
 densityKey.textContent = 'Solar Wind Density:'
-bzKey.textContent = 'Solar Wind Direction:'
+bzKey.textContent = 'Solar Wind Magnetic Field:'
 intensityKey.textContent = 'Calculated Intensity:'
+
+let speedUnits = document.querySelector('#speed > .units')
+let densityUnits = document.querySelector('#density > .units')
+let bzUnits = document.querySelector('#bz > .units')
+
+
+speedUnits.textContent = 'km/s'
+densityUnits.textContent = 'p/cm'+'\u00B3' 
+bzUnits.textContent = 'nT'
+
+
 
 fetch(baseUrl + '/products/solar-wind/plasma-1-day.json')
   .then(function(response) {
@@ -127,9 +159,9 @@ var bz = 0
 header.appendChild(newh2);
 
 var intensity = document.createElement('div')
-const speedValue = document.querySelector('#speed > #value')
-const densityValue = document.querySelector('#density > #value')
-const bzValue = document.querySelector('#bz > #value')
-const intensityValue = document.querySelector('#intensity > #value')
+const speedValue = document.querySelector('#speed > .value')
+const densityValue = document.querySelector('#density > .value')
+const bzValue = document.querySelector('#bz > .value')
+const intensityValue = document.querySelector('#intensity > .value')
 
 
